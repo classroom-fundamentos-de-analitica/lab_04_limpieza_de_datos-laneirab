747f5293cda1c46381668e31e5388141ebd22f2b
Laura  Melissa <laneirab@unal.edu.co>
2024-08-20 11:39:17 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -7,39 +7,42 @@ correctamente. Tenga en cuenta datos faltantes y duplicados.
 
 """
 
+"""
+Limpieza de datos usando Pandas
+-----------------------------------------------------------------------------------------
+
+Realice la limpieza del dataframe. Los tests evaluan si la limpieza fue realizada 
+correctamente. Tenga en cuenta datos faltantes y duplicados.
+
+"""
+
 import pandas as pd
 
-def procesar_datos():
-    # Cargar datos desde archivo CSV
-    datos = pd.read_csv("solicitudes_credito.csv", sep=";", index_col=0)
 
-    # Reemplazar caracteres no deseados y estandarizar texto a minúsculas
-    datos = datos.replace(["-", "_"], " ", regex=True)
-    datos = datos.applymap(lambda x: x.lower() if isinstance(x, str) else x)
+def clean_data():
+    df = pd.read_csv("solicitudes_credito.csv", sep=";", index_col=0)
 
-    # Transformar columna monto_del_credito a tipo numérico
-    datos["monto_del_credito"] = (
-        datos["monto_del_credito"]
-        .str.strip()
-        .str.replace(r"[,$]|(\.00$)", "", regex=True)
+    df = df.replace("-", " ", regex=True).replace("_", " ", regex=True)
+    df = df.applymap(lambda x: x.lower() if isinstance(x, str) else x)
+
+    df["monto_del_credito"] = (
+        df["monto_del_credito"]
+        .str.strip(" ")
+        .str.replace("[,$]|(\.00$)", "", regex=True)
         .astype(float)
     )
 
-    # Convertir columna fecha_de_beneficio a formato de fecha (dos intentos de formato)
-    datos["fecha_de_beneficio"] = pd.to_datetime(
-        datos["fecha_de_beneficio"], format="%d/%m/%Y", errors="coerce"
-    )
-    datos["fecha_de_beneficio"].fillna(
-        pd.to_datetime(datos["fecha_de_beneficio"], format="%Y/%m/%d", errors="coerce"), inplace=True
+    df["fecha_de_beneficio"] = pd.to_datetime(
+        df["fecha_de_beneficio"], format="%d/%m/%Y", errors="coerce"
+    ).fillna(
+        pd.to_datetime(df["fecha_de_beneficio"], format="%Y/%m/%d", errors="coerce")
     )
 
-    # Convertir comuna_ciudadano a enteros, pero verificando que sean válidos
-    datos = datos[datos["comuna_ciudadano"].notna()]  # Eliminar nulos antes de convertir
-    datos["comuna_ciudadano"] = datos["comuna_ciudadano"].astype(int)
+    df["comuna_ciudadano"] = df["comuna_ciudadano"].astype(int)
 
-    # Eliminar duplicados y registros con valores nulos
-    datos = datos.drop_duplicates().dropna()
+    df = df.drop_duplicates().dropna()
 
-    return datos
+    return df
 
-procesar_datos()
+
+clean_data()
